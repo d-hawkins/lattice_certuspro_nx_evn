dcea067253d6c41f3be676d49f9ee7bb60d5997c
David Hawkins <David.W.Hawkins@jpl.nasa.gov>
2025-05-04 19:24:09 -0700

    Blinky green LEDs design

---
+++ designs/blinky_g/scripts/parameters.tcl
@@ -0,0 +1,108 @@
+# -----------------------------------------------------------------------------
+# parameters.tcl
+#
+# 5/3/2025 D. W. Hawkins (dwh@caltech.edu)
+#
+# Design parameters utilities.
+#
+# This script is required in each of the design scripts directories.
+# The common procedures are located in $repo/tcl/constraints_utils.tcl.
+# The pre-synthesis script $repo/tcl/radiant_hdl_param.tcl reads the
+# project HDL_PARAM setting and uses it to update the timing parameters.
+#
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Get Timing Parameters
+# -----------------------------------------------------------------------------
+#
+# The design timing parameters change depending on the top-level parameter
+# USEIOFF. The top-level parameter can be changed using the Radiant GUI, so
+# Radiant is configured to run a pre-synthesis script that reads USEIOFF,
+# and then writes the design timing constraints to a YAML file in the build
+# directory.
+#
+# The LED clock-to-output constraints were optimized for the Green LEDs. The
+# Red and Yellow pass timing with these same constraints but the slack
+# changes.
+#
+proc get_timing_parameters {useioff} {
+
+	# Clock frequency
+	set clk_12mhz_frequency 12.000e6
+
+	if {$useioff == 1} {
+		#
+		#  I/O register delays at 0C
+		#  -----------------------------
+		# | Delay Type |   G+R+Y LEDs   |
+		# |            |  Min     Max   |
+		# |------------|----------------|
+		# | Actual     | 4.669   9.173  |
+		# | Constraint | 4.500   9.300  |
+		# | Slack      | 0.169   0.127  |
+		# | LED        | g[3]    g[7]   |
+		#  -----------------------------
+		#
+		set params [list \
+			USEIOFF              $useioff             \
+			CLK_12MHZ_FREQUENCY  $clk_12mhz_frequency \
+			LED_OUTPUT_MIN       4.5                  \
+			LED_OUTPUT_MAX       9.3                  \
+		]
+	} else {
+		#
+		#  Fabric register delays at 0C
+		#  -----------------------------
+		# | Delay Type |   G+R+Y LEDs   |
+		# |            |  Min     Max   |
+		# |------------|----------------|
+		# | Actual     | 4.665   9.746  |
+		# | Constraint | 4.500   9.900  |
+		# | Slack      | 0.165   0.154  |
+		# | LED        | g[3]    g[4]   |
+		#  -----------------------------
+		#
+		set params [list \
+			USEIOFF              $useioff             \
+			CLK_12MHZ_FREQUENCY  $clk_12mhz_frequency \
+			LED_OUTPUT_MIN       4.5                  \
+			LED_OUTPUT_MAX       9.9                  \
+		]
+	}
+	return $params
+}
+
+# -----------------------------------------------------------------------------
+# HDL_PARAM update
+# -----------------------------------------------------------------------------
+#
+# The pre-script sources the project parameters.tcl script and calls
+#
+# > parameters_update $repo $board $design $build $hdl_param
+#
+# Each design creates a custom parameters.tcl with a customized callback
+# to implement whatever project-specific changes are required.
+#
+# This procedure updates the SDC constraints based on USEIOFF.
+#
+proc parameters_update {repo board design build hdl_param} {
+
+	# Extract the USEIOFF setting
+	set useioff [get_hdl_param $hdl_param USEIOFF]
+
+	# Update the YAML file
+	set filename $build/parameters.yml
+	yaml_parameters_update $filename $hdl_param
+
+	# Update the SDC file
+	#  * The SDC file needs to already exist in the build directory
+	#  * The prj_source_add command does not work from a pre-script
+	#
+	# SDC template and project files
+	set filename_i $repo/designs/$design/scripts/${board}_sdc.txt
+	set filename_o $build/${board}.sdc
+	update_sdc_file $filename_i $filename_o $useioff
+
+	return
+}
---
+++ designs/blinky_g/src/certuspro_nx_evn.sv
@@ -0,0 +1,111 @@
+// ----------------------------------------------------------------------------
+// certuspro_nx_evn.sv
+//
+// 4/25/2025 D. W. Hawkins (dwh@caltech.edu)
+//
+// Lattice CertusPro-NX Evaluation Board 'blinky' design.
+//
+// ----------------------------------------------------------------------------
+// References
+// ----------
+//
+// [1] Lattice, "CertusPro-NX Evaluation Board", 2025
+//     https://www.latticesemi.com/products/developmentboardsandkits/certuspro-nxevaluationboard
+//     Ordering Part Number: LFCPNX-EVN
+//
+// ----------------------------------------------------------------------------
+
+module certuspro_nx_evn #(
+		// Use I/O Register
+		parameter int USEIOFF = 1
+	) (
+
+		// --------------------------------------------------------------------
+		// Clock
+		// --------------------------------------------------------------------
+		//
+		input        clk_12mhz,
+
+		// --------------------------------------------------------------------
+		// User I/O
+		// --------------------------------------------------------------------
+		//
+		// LEDs
+		output [7:0] led
+	);
+
+	// ------------------------------------------------------------------------
+	// Local parameters
+	// ------------------------------------------------------------------------
+	//
+	// Clock frequency
+	localparam real CLK_12MHZ_FREQUENCY = 12.0e6;
+
+	// LED blink rate
+	localparam real BLINK_PERIOD = 0.5;
+
+	// Counter width
+	//
+	// 8 LEDs driven by the 12MHz clock
+	localparam integer WIDTH =
+		$clog2(integer'(CLK_12MHZ_FREQUENCY*BLINK_PERIOD))+7;
+
+	// ------------------------------------------------------------------------
+	// Local signals
+	// ------------------------------------------------------------------------
+	//
+	// Counter
+	logic [WIDTH-1:0] count;
+
+	// ------------------------------------------------------------------------
+	// Counter
+	// ------------------------------------------------------------------------
+	//
+	// The LEDs are turned on using a logic low level.
+	// Use a down counter, so that the LEDs show an up count.
+	//
+	always_ff @(posedge clk_12mhz) begin
+		count <= count - 1'b1;
+	end
+
+	// ------------------------------------------------------------------------
+	// LED outputs
+	// ------------------------------------------------------------------------
+	//
+	// Pipelined through registers, so the registers can be placed near the
+	// output pins to investigate clock-to-output delay.
+	//
+	// Lattice Radiant does not have a .PDC constraint for using I/O registers,
+	// so the parameter USEIOFF was added to the design to allow a parameter to
+	// control the use of the I/O registers.
+	//
+	// To change the USEIOFF parameter:
+	// * Select "Project > Active Implementation > Set Top-Level Unit"
+	// * Set "HDL Parameters" to USEIOFF=0 or USEIOFF=1
+	//
+	// The equivalent Tcl is:
+	// > prj_set_impl_opt -impl "impl_1" {HDL_PARAM} {USEIOFF=0}
+	// > prj_set_impl_opt -impl "impl_1" {HDL_PARAM} {USEIOFF=1}
+	//
+	// HDL_PARAM can have multiple values separated by semicolons.
+
+	// Fabric Register
+	if (USEIOFF == 0) begin: g0
+		logic [7:0] led_iob /* synthesis syn_useioff = 0 */;
+		always_ff @(posedge clk_12mhz) begin
+			led_iob <= count[(WIDTH-1) -: 8];
+		end
+		assign led = led_iob;
+	end
+
+	// I/O Register
+	if (USEIOFF == 1) begin: g1
+		logic [7:0] led_iob /* synthesis syn_useioff = 1 */;
+		always_ff @(posedge clk_12mhz) begin
+			led_iob <= count[(WIDTH-1) -: 8];
+		end
+		assign led = led_iob;
+	end
+
+endmodule
+
