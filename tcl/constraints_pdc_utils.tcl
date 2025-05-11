4898161367f302b2389e60f27ebf95a08426b5fe
David Hawkins <David.W.Hawkins@jpl.nasa.gov>
2025-05-11 16:52:24 -0700

    Reverse LED bit order

--- tcl/constraints_pdc_utils.tcl
+++ tcl/constraints_pdc_utils.tcl
@@ -71,40 +71,77 @@ proc get_pin_constraints {} {
 	# Green LEDs
 	# -------------------------------------------------------------------------
 	#
-	dict set pin led_g(0)  {PIN N5  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_g(1)  {PIN N6  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_g(2)  {PIN N7  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_g(3)  {PIN N8  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_g(4)  {PIN L6  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_g(5)  {PIN N9  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_g(6)  {PIN L8  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_g(7)  {PIN M9  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	# The bit order is reversed relative to the schematic, so the LSB on the
+	# PCB is on the right-side of the 8 LEDs.
+	#
+	dict set pin led_g(7)  {PIN N5  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_g(6)  {PIN N6  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_g(5)  {PIN N7  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_g(4)  {PIN N8  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_g(3)  {PIN L6  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_g(2)  {PIN N9  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_g(1)  {PIN L8  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_g(0)  {PIN M9  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
 
 	# -------------------------------------------------------------------------
 	# Red LEDs
 	# -------------------------------------------------------------------------
 	#
-	dict set pin led_r(0)  {PIN T4  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_r(1)  {PIN T5  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_r(2)  {PIN T6  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_r(3)  {PIN T7  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_r(4)  {PIN U8  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_r(5)  {PIN T8  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_r(6)  {PIN R9  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_r(7)  {PIN P9  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	# The bit order is reversed relative to the schematic, so the LSB on the
+	# PCB is on the right-side of the 8 LEDs.
+	#
+	dict set pin led_r(7)  {PIN T4  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_r(6)  {PIN T5  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_r(5)  {PIN T6  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_r(4)  {PIN T7  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_r(3)  {PIN U8  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_r(2)  {PIN T8  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_r(1)  {PIN R9  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_r(0)  {PIN P9  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
 
 	# -------------------------------------------------------------------------
 	# Yellow LEDs
 	# -------------------------------------------------------------------------
 	#
-	dict set pin led_y(0)  {PIN N1  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_y(1)  {PIN N2  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_y(2)  {PIN N3  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_y(3)  {PIN M1  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_y(4)  {PIN M2  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_y(5)  {PIN M3  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_y(6)  {PIN L3  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
-	dict set pin led_y(7)  {PIN N4  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	# The bit order is reversed relative to the schematic, so the LSB on the
+	# PCB is on the right-side of the 8 LEDs.
+	#
+	dict set pin led_y(7)  {PIN N1  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_y(6)  {PIN N2  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_y(5)  {PIN N3  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_y(4)  {PIN M1  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_y(3)  {PIN M2  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_y(2)  {PIN M3  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_y(1)  {PIN L3  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+	dict set pin led_y(0)  {PIN N4  IO_TYPE LVCMOS33  SLEWRATE MED  DRIVE 8}
+
+	# -------------------------------------------------------------------------
+	# Push Buttons
+	# -------------------------------------------------------------------------
+	#
+	# * Bank 0 at 1.8V (p4 [2])
+	# * Radiant "Physical Designer" shows GLITCHFILTER = ON, HYSTERESIS = ON
+	#
+	dict set pin pb(0)  {PIN J5  IO_TYPE LVCMOS18}
+	dict set pin pb(1)  {PIN J2  IO_TYPE LVCMOS18}
+	dict set pin pb(2)  {PIN J3  IO_TYPE LVCMOS18}
+
+	# -------------------------------------------------------------------------
+	# DIP Switches
+	# -------------------------------------------------------------------------
+	#
+	# * Bank 0 at 1.8V (p4 [2])
+	# * Radiant "Physical Designer" shows GLITCHFILTER = ON, HYSTERESIS = ON
+	# * The LSB on the PCB is on the right-hand side (closest to the SMAs)
+	#
+	dict set pin sw(0)  {PIN K8  IO_TYPE LVCMOS18}
+	dict set pin sw(1)  {PIN K7  IO_TYPE LVCMOS18}
+	dict set pin sw(2)  {PIN K6  IO_TYPE LVCMOS18}
+	dict set pin sw(3)  {PIN K4  IO_TYPE LVCMOS18}
+	dict set pin sw(4)  {PIN K3  IO_TYPE LVCMOS18}
+	dict set pin sw(5)  {PIN K2  IO_TYPE LVCMOS18}
+	dict set pin sw(6)  {PIN J7  IO_TYPE LVCMOS18}
+	dict set pin sw(7)  {PIN J6  IO_TYPE LVCMOS18}
 
 	return $pin
 }
@@ -129,8 +166,16 @@ proc get_pin_constraints {} {
 #
 proc apply_pin_constraints {ports pin_constraints} {
 
+	# JTAG pins
+	set jtag_ports [list TCK TMS TDI TDO]
+
 	# Loop over the top-level ports list
 	foreach port $ports {
+
+		# Skip JTAG pins (they are auto-assigned)
+		if {[lsearch $jtag_ports $port] != -1} {
+			continue
+		}
 
 		# Convert port name (with square brackets) to pin name
 		# * Replace square brackets with paranthesis
