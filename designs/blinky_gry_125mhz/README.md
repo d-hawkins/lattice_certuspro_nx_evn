2703638914495cee53de478f418fffbbfffef113
David Hawkins <David.W.Hawkins@jpl.nasa.gov>
2025-05-16 17:22:23 -0700

    Blinky G+R+Y LEDs using 125MHz

---
+++ designs/blinky_gry_125mhz/README.md
@@ -0,0 +1,38 @@
+# Lattice Semiconductor CertusPro-NX Evaluation Board 'blinky_gry_125mhz' design.
+
+5/3/2025 D. W. Hawkins (dwh@caltech.edu)
+
+## Introduction
+
+The 'blinky_gry_125mhz' design blinks the Green, Red, and Yellow LEDs using the 125MHz on-board oscillator.
+
+The top-level design parameter USEIOFF controls whether the LED outputs are registered using I/O Registers (USEIOFF=1) or fabric registers (USEIOFF=0). The SDC timing constraints are configured with clock-to-output minimum and maximum values that result in under 0.2ns of slack. The clock-to-output delays differ for the two different USEIOFF settings. The build script (scripts/radiant.tcl) generates two project variants: (1) USEIOFF = 1 and (2) USEIOFF = 0. The build script generates SDC constraints files in each of the build directories. The USEIOFF parameter can be changed in the Radiant GUI using "Project > Active Implementation > Set Top-Level Unit" and then editing the "HDL Parameters" field, eg., USEIOFF=1. The project build script defines USEIOFF for each of the project variants, but once the projects are open using the Radiant GUI, the value of USEIOFF can be changed, and the project rebuilt. During build, a pre-synthesis script reads the HDL parameters GUI value, extracts USEIOFF, and updates the SDC constraints.
+
+-------------------------------------------------------------------------------
+# Bitstream Generation
+
+1. Start the Lattice Radiant 2024.2 Tcl Console
+
+2. Change directory to the project and run the project setup and build script  
+   ~~~
+   cd c:/github/lattice_certuspro_nx_evn/designs/blinky_gry_125mhz
+   source scripts/radiant.tcl
+   ~~~  
+   The Radiant Tcl console is used, as the Radiant GUI will not display a Tcl console until a project is open.  
+   Exit from the Radiant Tcl console when the script completes.  
+   The script builds two design variants.
+
+3. Start the Lattice Radiant 2024.2 GUI  
+   Use "File > Open > Project" to open the Radiant project file (.rdf) for a build variant.
+
+4. Timing Analysis  
+   Select "Tools > Timing Analyzer", or click on the "Timing Analyzer" tool on the top-edge of the GUI.  
+   Click on the "Query" tab, add the LEDs to the analysis, change the temperature to 0C, and click "Search".  
+   This displays the worst-case timing slack. The 85C timing has higher setup slack, but the same hold slack (as the GUI does not update the hold timing).  
+   Click on the worst-case setup or hold path to see the clock and data delays.  
+   The timing should match the comments in the parameters.tcl file.
+
+5. Program the board.  
+   Power-up and connect to the USB interface on the CertusPro-NX Evaluation Board.  
+   In the Radiant GUI, select "Tools > Programmer", or click on the "Programmer" tool on the top-edge of the GUI.  
+   In the Radiant Programmer, select "Run > Program Device", or click on the "Program Device" tool on the top-edge of the GUI.
---
+++ designs/blinky_gry_125mhz/src/certuspro_nx_evn.sv
@@ -0,0 +1,149 @@
+// ----------------------------------------------------------------------------
+// certuspro_nx_evn.sv
+//
+// 4/25/2025 D. W. Hawkins (dwh@caltech.edu)
+//
+// Lattice CertusPro-NX Evaluation Board 'blinky_gry' design.
+//
+// Blink the green, red, and yellow LEDs.
+//
+// ----------------------------------------------------------------------------
+// References
+// ----------
+//
+// [1] Lattice, "CertusPro-NX Evaluation Board", 2025
+//     https://www.latticesemi.com/products/developmentboardsandkits/certuspro-nxevaluationboard
+//     Ordering Part Number: LFCPNX-EVN
+//
+// ----------------------------------------------------------------------------
+
+module certuspro_nx_evn #(
+		// Use I/O Register
+		parameter int USEIOFF = 1
+	) (
+
+		// --------------------------------------------------------------------
+		// Clock
+		// --------------------------------------------------------------------
+		//
+		input        clk_125mhz,
+
+		// --------------------------------------------------------------------
+		// User I/O
+		// --------------------------------------------------------------------
+		//
+		// LEDs
+		output [7:0] led_g,
+		output [7:0] led_r,
+		output [7:0] led_y
+	);
+
+	// ------------------------------------------------------------------------
+	// Local parameters
+	// ------------------------------------------------------------------------
+	//
+	// Clock frequency
+	localparam real CLK_125MHZ_FREQUENCY = 125.0e6;
+
+	// LED blink rate
+	localparam real BLINK_PERIOD = 0.5;
+
+	// Counter width
+	//
+	// 8 LEDs driven by the 12MHz clock
+	localparam integer WIDTH =
+		$clog2(integer'(CLK_125MHZ_FREQUENCY*BLINK_PERIOD))+7;
+
+	// ------------------------------------------------------------------------
+	// Local signals
+	// ------------------------------------------------------------------------
+	//
+	// Counter
+	logic [WIDTH-1:0] count;
+
+	// LED brightness control
+	logic led_duty;
+
+	// LED fabric registers
+	logic [7:0] led_fabric;
+
+	// ------------------------------------------------------------------------
+	// Counter
+	// ------------------------------------------------------------------------
+	//
+	// The LEDs are turned on using a logic low level.
+	// Use a down counter, so that the LEDs show an up count.
+	//
+	always_ff @(posedge clk_125mhz) begin
+		count <= count - 1'b1;
+	end
+
+	// ------------------------------------------------------------------------
+	// LED brightness control
+	// ------------------------------------------------------------------------
+	//
+	// 12.5% duty-cycle
+	assign led_duty = (count[(WIDTH-16) +: 3] == 3'h0) ? 1'b1 : 1'b0;
+
+	// ------------------------------------------------------------------------
+	// LED fabric registers
+	// ------------------------------------------------------------------------
+	//
+	// Reduce the brightness of the Green LEDs
+	always_ff @(posedge clk_125mhz) begin
+		led_fabric <= led_duty ? count[(WIDTH-1) -: 8] : 8'hFF;
+	end
+
+	// ------------------------------------------------------------------------
+	// LED outputs
+	// ------------------------------------------------------------------------
+	//
+	// Pipelined through registers, so the registers can be placed near the
+	// output pins to investigate clock-to-output delay.
+	//
+	// Lattice Radiant does not have a .PDC constraint for using I/O registers,
+	// so the parameter USEIOFF was added to the design to allow a parameter to
+	// control the use of the I/O registers.
+	//
+	// To change the USEIOFF parameter:
+	// * Select "Project > Active Implementation > Set Top-Level Unit"
+	// * Set "HDL Parameters" to USEIOFF=0 or USEIOFF=1
+	//
+	// The equivalent Tcl is:
+	// > prj_set_impl_opt -impl "impl_1" {HDL_PARAM} {USEIOFF=0}
+	// > prj_set_impl_opt -impl "impl_1" {HDL_PARAM} {USEIOFF=1}
+	//
+	// HDL_PARAM can have multiple values separated by semicolons.
+
+	// Fabric Register
+	if (USEIOFF == 0) begin: g0
+		logic [7:0] led_iob_g /* synthesis syn_preserve = 1 syn_useioff = 0 */;
+		logic [7:0] led_iob_r /* synthesis syn_preserve = 1 syn_useioff = 0 */;
+		logic [7:0] led_iob_y /* synthesis syn_preserve = 1 syn_useioff = 0 */;
+		always_ff @(posedge clk_125mhz) begin
+			led_iob_g <= led_fabric;
+			led_iob_r <= led_fabric;
+			led_iob_y <= led_fabric;
+		end
+		assign led_g = led_iob_g;
+		assign led_r = led_iob_r;
+		assign led_y = led_iob_y;
+	end
+
+	// I/O Register
+	if (USEIOFF == 1) begin: g1
+		logic [7:0] led_iob_g /* synthesis syn_preserve = 1 syn_useioff = 1 */;
+		logic [7:0] led_iob_r /* synthesis syn_preserve = 1 syn_useioff = 1 */;
+		logic [7:0] led_iob_y /* synthesis syn_preserve = 1 syn_useioff = 1 */;
+		always_ff @(posedge clk_125mhz) begin
+			led_iob_g <= led_fabric;
+			led_iob_r <= led_fabric;
+			led_iob_y <= led_fabric;
+		end
+		assign led_g = led_iob_g;
+		assign led_r = led_iob_r;
+		assign led_y = led_iob_y;
+	end
+
+endmodule
+
